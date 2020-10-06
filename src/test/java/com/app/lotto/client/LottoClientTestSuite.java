1c34b6e6c474be0ad5f251c3a4bf2d51adbfd09d
kkazm <s14005@pjwstk.edu.pl>
2020-10-06 13:40:47 +0200

    Add second test.

--- src/test/java/com/app/lotto/client/LottoClientTestSuite.java
+++ src/test/java/com/app/lotto/client/LottoClientTestSuite.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -22,5 +23,19 @@ public class LottoClientTestSuite {
         assertNotNull(url);
         assertEquals("https://test/com?key1=value1&key2=value2", url.toString());
         logger.info(url.toString());
+    }
+
+    @Test
+    void testUriComponentsBuilder2() {
+        URI url = UriComponentsBuilder.fromHttpUrl("https://www.lotto.pl/api/lotteries/draw-results/by-gametype")
+                .queryParam("game", "Lotto")
+                .queryParam("index", 1)
+                .queryParam("size", 1)
+                .queryParam("sort", "drawDate")
+                .queryParam("order", "DESC")
+                .build().encode().toUri();
+        logger.info(url.toString());
+
+        assertEquals("https://www.lotto.pl/api/lotteries/draw-results/by-gametype?game=Lotto&index=1&size=1&sort=drawDate&order=DESC", url.toString());
     }
 }
