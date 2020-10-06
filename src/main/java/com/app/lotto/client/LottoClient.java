834e15dc5c7b20b751ae573fa5e8ac046421c6c7
kkazm <s14005@pjwstk.edu.pl>
2020-10-06 12:53:16 +0200

    Implement getLastResult.

--- src/main/java/com/app/lotto/client/LottoClient.java
+++ src/main/java/com/app/lotto/client/LottoClient.java
@@ -1,15 +1,25 @@
 package com.app.lotto.client;
 
+import com.app.lotto.domain.Result;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @Component
 public class LottoClient {
+    private final Logger logger = LoggerFactory.getLogger(LottoClient.class);
+
     @Value("${lotto.api.endpoint}")
     private String endpoint;
 
@@ -17,4 +27,16 @@ public class LottoClient {
 
     private RestTemplate restTemplate;
 
+    public Result[] getLastResult(String gameType){
+        URI url = UriComponentsBuilder.fromHttpUrl(endpoint)
+                .queryParam("game", gameType)
+                .queryParam("index", 1)
+                .queryParam("size", 1)
+                .queryParam("sort", "drawDate")
+                .queryParam("order", "DESC")
+                .build().encode().toUri();
+        logger.info(url.toString());
+        Result[] response = restTemplate.getForObject(url, Result[].class);
+        return response;
+    }
 }
