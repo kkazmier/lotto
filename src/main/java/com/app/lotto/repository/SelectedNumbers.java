df2ee7f54e807bc9f7a4c4b60594e17d11510c64
kkazm <s14005@pjwstk.edu.pl>
2020-10-08 21:28:25 +0200

    Add repository classes.

---
+++ src/main/java/com/app/lotto/repository/SelectedNumbers.java
@@ -0,0 +1,27 @@
+package com.app.lotto.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@Getter
+@Setter
+public class SelectedNumbers {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "gameType")
+    private String gameType;
+
+    @Column(name = "createdTime")
+    private LocalDateTime createdTime;
+}
---
+++ src/main/java/com/app/lotto/repository/SelectedNumbersRepository.java
@@ -0,0 +1,7 @@
+package com.app.lotto.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SelectedNumbersRepository extends CrudRepository<SelectedNumbers, Long> {
+
+}
