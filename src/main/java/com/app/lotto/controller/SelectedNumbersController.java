037fb60588669193d5e148561481ace8c3e2bbb1
kkazm <s14005@pjwstk.edu.pl>
2020-10-09 21:02:41 +0200

    Create empty class.

---
+++ src/main/java/com/app/lotto/controller/SelectedNumbersController.java
@@ -0,0 +1,9 @@
+package com.app.lotto.controller;
+
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("")
+@NoArgsConstructor
+public class SelectedNumbersController {
+}
