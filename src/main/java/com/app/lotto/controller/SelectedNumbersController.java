77eeaa8c8e886a60a6709345d2f3e642010e6e16
kkazm <s14005@pjwstk.edu.pl>
2020-10-11 16:23:20 +0200

    Implement getAll and createNumbers.

--- src/main/java/com/app/lotto/controller/SelectedNumbersController.java
+++ src/main/java/com/app/lotto/controller/SelectedNumbersController.java
@@ -1,9 +1,33 @@
 package com.app.lotto.controller;
 
+import com.app.lotto.repository.SelectedNumbers;
+import com.app.lotto.service.SelectedNumbersService;
+import com.app.lotto.service.SelectedNumbersServiceImpl;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("")
-@NoArgsConstructor
+import java.util.List;
+
+@RestController
+@RequestMapping("")
+@AllArgsConstructor
 public class SelectedNumbersController {
+    private final Logger logger = LoggerFactory.getLogger(SelectedNumbersController.class);
+    private final SelectedNumbersService service;
+
+    @GetMapping(value = "all")
+    List<SelectedNumbers> getAll(){
+        return service.getAll();
+    }
+
+    @PostMapping(value = "create", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public SelectedNumbers createNumbers(@RequestBody SelectedNumbers numbers){
+        logger.info("Add " + numbers);
+        return service.save(numbers);
+    }
 }
