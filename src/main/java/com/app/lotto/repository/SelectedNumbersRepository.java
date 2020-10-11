4ed39292dbcaeaeaad45f7666cc8e9d0f6b0a0cb
kkazm <s14005@pjwstk.edu.pl>
2020-10-11 16:23:55 +0200

    Add annotations.

--- src/main/java/com/app/lotto/repository/SelectedNumbersRepository.java
+++ src/main/java/com/app/lotto/repository/SelectedNumbersRepository.java
@@ -1,9 +1,13 @@
 package com.app.lotto.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Repository
+@Transactional
 public interface SelectedNumbersRepository extends CrudRepository<SelectedNumbers, Long> {
     List<SelectedNumbers> findAll();
     SelectedNumbers save(SelectedNumbers numbers);
