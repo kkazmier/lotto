65a72d9ce526a8576259cf4838ed45572d0a6a81
kkazm <s14005@pjwstk.edu.pl>
2020-10-11 16:24:21 +0200

    Add annotation.

--- src/main/java/com/app/lotto/service/SelectedNumbersService.java
+++ src/main/java/com/app/lotto/service/SelectedNumbersService.java
@@ -1,11 +1,9 @@
 package com.app.lotto.service;
 
 import com.app.lotto.repository.SelectedNumbers;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface SelectedNumbersService {
     List<SelectedNumbers> getAll();
     SelectedNumbers save(SelectedNumbers numbers);
