24884c4f9a53719ae718210b0a023b95b3189425
kkazm <s14005@pjwstk.edu.pl>
2020-10-17 01:05:02 +0200

    Implement findHitQuantity and convertToIntegersSet, add logger.

--- src/main/java/com/app/lotto/logic/WinsAnalyser.java
+++ src/main/java/com/app/lotto/logic/WinsAnalyser.java
@@ -1,13 +1,41 @@
 package com.app.lotto.logic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinsAnalyser {
-    public List<Wins> findLottoGameWins(List<String> numbers){
-        List<Wins> result = new ArrayList<>();
+    Logger logger = LoggerFactory.getLogger(WinsAnalyser.class);
+
+    public List<Integer> findLottoGameWins(String result, List<String> numbers){
+        List<Integer> wins = new ArrayList<>();
 
 
+        return wins;
+    }
+
+    public Integer findHitQuantity(String result, String numbers){
+        Set<Integer> resultSet = convertToIntegersSet(result);
+        Set<Integer> numbersSet = convertToIntegersSet(numbers);
+        final int[] hitsQuantity = {0};
+        numbersSet.forEach(n -> {
+            if(resultSet.contains(n)){
+                hitsQuantity[0]++;
+            }
+        });
+        logger.info("Number of hits: " + hitsQuantity[0]);
+        return hitsQuantity[0];
+    }
+
+    public Set<Integer> convertToIntegersSet(String numbers){
+        Set<Integer> result = new HashSet<>();
+        for (int i=0; i<numbers.length(); i+=2){
+            result.add(new Integer(numbers.substring(i, i+2)));
+        }
         return result;
     }
 }
