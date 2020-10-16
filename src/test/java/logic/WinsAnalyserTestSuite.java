1196bd421c9c34fbfe6aa9e53f0da342c7846287
kkazm <s14005@pjwstk.edu.pl>
2020-10-17 01:35:08 +0200

    Implement testFindLottoGameHits.

--- src/test/java/logic/WinsAnalyserTestSuite.java
+++ src/test/java/logic/WinsAnalyserTestSuite.java
@@ -3,7 +3,9 @@ package logic;
 import com.app.lotto.logic.WinsAnalyser;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,5 +35,21 @@ public class WinsAnalyserTestSuite {
         assertEquals(4, analyser.findHitQuantity(resultNumbers, "052204025501"));
         assertEquals(5, analyser.findHitQuantity(resultNumbers, "050301020611"));
         assertEquals(6, analyser.findHitQuantity(resultNumbers, "060504010203"));
+    }
+
+    @Test
+    void testFindLottoGameHits() {
+        String resultNumbers = "010203040506";
+        List<String> numbers = new ArrayList<>();
+        numbers.add("061122334455");
+        numbers.add("060433445501");
+        numbers.add("050301020611");
+
+        List<Integer> hits = analyser.findLottoGameHits(resultNumbers, numbers);
+
+        assertEquals(3, hits.size());
+        assertEquals(1, hits.get(0));
+        assertEquals(3, hits.get(1));
+        assertEquals(5, hits.get(2));
     }
 }
