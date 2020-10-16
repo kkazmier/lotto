9cb7b6be3a5e724d526b0a2b9bca76833e269b57
kkazm <s14005@pjwstk.edu.pl>
2020-10-17 01:03:59 +0200

    Add fields.

---
+++ src/main/java/com/app/lotto/logic/Hits.java
@@ -0,0 +1,5 @@
+package com.app.lotto.logic;
+
+public enum Hits {
+    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX
+}
--- src/main/java/com/app/lotto/logic/Wins.java
+++
@@ -1,5 +0,0 @@
-package com.app.lotto.logic;
-
-public enum Wins {
-    THREE, FOUR, FIVE, SIX
-}
